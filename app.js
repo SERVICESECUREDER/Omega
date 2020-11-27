b1bd0692486dfbc0fa37e7e9bf15b0a9b4606d0c
SERVICESECUREDER <75127434+SERVICESECUREDER@users.noreply.github.com>
2020-11-27 18:53:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,16 @@
 
 ///Edit Under This only
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+var // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB7JFjHbd8D4XImCX94eALgv7BDk-YSkUo",
+  authDomain: "instagram-88edb.firebaseapp.com",
+  databaseURL: "https://instagram-88edb.firebaseio.com",
+  projectId: "instagram-88edb",
+  storageBucket: "instagram-88edb.appspot.com",
+  messagingSenderId: "901413815857",
+  appId: "1:901413815857:web:c47948af0eaa853231b710",
+  measurementId: "G-KSN978573P"
 };
 
      
